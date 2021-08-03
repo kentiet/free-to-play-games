219c605eaf041a7c0f2eb0bb6f85eac28d392931
Ken Tiet <ken.tietkien@gmail.com>
2021-08-02 18:00:59 -0700

    add the local json db file in case the cors from public api is not allowed to be used

--- src/App.js
+++ src/App.js
@@ -1,37 +1,52 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import CardList from './components/card-list/card-list.component'
-import ErrorBoundry from './components/ErrorBoundry.component'
-import SearchField from './components/search/search.component'
+import CardList from './components/card-list/card-list.component';
+import ErrorBoundry from './components/ErrorBoundry.component';
+import SearchField from './components/search/search.component';
+import data from './db.json'
 // import FilterTool from './components/filter-tool/filter-tool.component'
 
 function App() {
+  const [games, setGames] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  const[games, setGames] = useState([])
-  const[searchField, setSearchField] = useState('')
-
-  useEffect(()=>{
+  const fetchData = () => {
     fetch('https://cors.bridged.cc/https://freetogame.com/api/games')
-    .then(res => res.json())
-    .then(games => setGames(games))
-  },[])
+      .then((res) => {
+        if (res.status === 200) return res.json();
+        else {
+          const loadData = JSON.stringify(data)
+          const json = JSON.parse(loadData)
+          return json
+        }
+      })
+      .then((games) => setGames(games))
+      .catch((err) => {
+        console.error({
+          message: `can't load the data from this API, error: ${err}`,
+        });
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const onSearchChange = (event) => {
-    setSearchField(event.target.value)
-  }
+    setSearchField(event.target.value);
+  };
 
-  const filteredGames = games.filter(game => {
-    return game.title.toLowerCase().includes(searchField.toLowerCase())
-  })
+  const filteredGames = games.filter((game) => {
+    return game.title.toLowerCase().includes(searchField.toLowerCase());
+  });
 
-  
   return (
     <div className="App">
-      <h1 className='title'>Free To Play Games</h1>
-      <SearchField onSearchChange={onSearchChange}/>
+      <h1 className="title">Free To Play Games</h1>
+      <SearchField onSearchChange={onSearchChange} />
       <ErrorBoundry>
-          <CardList games={filteredGames}/>
+        <CardList games={filteredGames} />
       </ErrorBoundry>
     </div>
   );
